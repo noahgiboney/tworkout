a752eb4f043ff8795a30f908a0acc97dafd268e3
Melika Dabiri <jalehdabiri@gmail.com>
2024-05-08 14:22:02 -0700

    installed chakra ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import styles from "./page.module.css";
+import { Box, Button, Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      
+    </>
   );
 }
