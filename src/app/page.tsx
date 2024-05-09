4f0cd039d84aa7151a221d9b97a98818e8e3da5b
Melika Dabiri <jalehdabiri@gmail.com>
2024-05-08 17:35:02 -0700

    made text and image bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         fontWeight="bold"
         color="white"
         textAlign="center"
-        fontSize={45}
+        fontSize={60}
         letterSpacing={2}
         marginBottom={5}
       >
@@ -30,26 +30,28 @@ export default function Home() {
       <Image
         src="/images/landing-page.webp"
         alt="man and woman working out"
-        width={700}
-        height={750}
+        width={800}
+        height={800}
       />
-      <Text margin={5} color="white" textAlign="center">
+      <Text margin={5} color="white" textAlign="center" fontSize={30}>
         Start your fitness journey today. Track, plan, and <br></br>visualize
         your progress all in one place.
       </Text>
       <Button
         borderRadius={10}
         padding={7}
-        width="130px"
+        fontSize={20}
+        width="140px"
         margin={3}
         onClick={() => handleRedirect(false)}
       >
         Login
       </Button>
       <Button
+        fontSize={20}
         borderRadius={10}
         padding={7}
-        width="130px"
+        width="140px"
         onClick={() => handleRedirect(true)}
       >
         Sign up
