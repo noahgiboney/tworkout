9b9713245528d21c0874a9dd7544fa06ebd98d33
sfishe10 <sfishe10@calpoly.edu>
2024-05-22 15:26:16 -0700

    basic cards and structure in place

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
+import styles from "./profile.module.css";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Text, 
+  Avatar
+} from "@chakra-ui/react";
 
 const Profile = () => {
   return (
-    <div><SideBar /></div>
+    <div className={styles.main}>
+      <div><SideBar /></div>
+      <div className={styles.body}>
+        <div className={styles.title}>
+          My Profile
+        </div>
+        <div className={styles.avatar}>
+          <Avatar size="superLg"/>
+        </div>
+        <div className={styles.cards}>
+          <Card marginTop="1rem" marginBottom="1rem" bgColor="#E9E4F2">
+            <CardHeader>
+              <Text color="black">
+                Email
+              </Text>
+            </CardHeader>
+          </Card>
+          <Card marginTop="1rem" marginBottom="1rem" bgColor="#E9E4F2">
+            <CardHeader>
+              <Text color="black">
+                Profile
+              </Text>
+            </CardHeader>
+          </Card>
+        </div>
+        
+      </div>
+      
+    </div>
   )
 }
 
