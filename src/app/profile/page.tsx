b4254f24775d3e237c4276976472501953872235
sfishe10 <sfishe10@calpoly.edu>
2024-05-23 07:46:38 -0700

    UI for profile page done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -18,22 +18,79 @@ const Profile = () => {
           My Profile
         </div>
         <div className={styles.avatar}>
-          <Avatar size="superLg"/>
+          <Avatar size="2xl"/>
         </div>
         <div className={styles.cards}>
           <Card marginTop="1rem" marginBottom="1rem" bgColor="#E9E4F2">
-            <CardHeader>
-              <Text color="black">
+            <CardHeader paddingBottom="0rem">
+              <Text fontSize={24} color="black">
                 Email
               </Text>
             </CardHeader>
+            <CardBody>
+              <Card paddingTop="0rem" bgColor="#C7B3DC">
+                <CardHeader>
+                  <Text fontSize={18} color="black">
+                    johndoe@gmail.com
+                  </Text>
+                </CardHeader>
+              </Card>
+            </CardBody>
           </Card>
           <Card marginTop="1rem" marginBottom="1rem" bgColor="#E9E4F2">
-            <CardHeader>
-              <Text color="black">
+            <CardHeader paddingBottom="0rem">
+              <Text fontSize={24} color="black">
                 Profile
               </Text>
             </CardHeader>
+            <CardBody>
+              <div className={styles.nameCards}>
+                <div className={styles.firstName}>
+                  <Text fontSize={18} color="black">
+                    First Name
+                  </Text>
+                  <Card paddingTop="0rem" bgColor="#C7B3DC">
+                    <CardHeader>
+                      John
+                    </CardHeader>
+                  </Card>
+                </div>
+                <div className={styles.lastName}>
+                  <Text fontSize={18} color="black">
+                    Last Name
+                  </Text>
+                  <Card paddingTop="0rem" bgColor="#C7B3DC">
+                    <CardHeader>
+                      Doe
+                    </CardHeader>
+                  </Card>
+                </div>
+              </div>
+              <Text paddingTop="1rem" fontSize={18} color="black">
+                Weight
+              </Text>
+              <Card bgColor="#C7B3DC">
+                <CardHeader>
+                  500 lbs
+                </CardHeader>
+              </Card>
+              <Text paddingTop="1rem" fontSize={18} color="black">
+                Height
+              </Text>
+              <Card bgColor="#C7B3DC">
+                <CardHeader>
+                  6'0"
+                </CardHeader>
+              </Card>
+              <Text paddingTop="1rem" fontSize={18} color="black">
+                Age
+              </Text>
+              <Card bgColor="#C7B3DC">
+                <CardHeader>
+                  45
+                </CardHeader>
+              </Card>
+            </CardBody>
           </Card>
         </div>
         
