0e97e06b043bbf19ec5b999f8d1a461be0b20d1c
Melika Dabiri <jalehdabiri@gmail.com>
2024-05-26 14:01:23 -0700

    disabled tiles

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -3,15 +3,28 @@ import React, { useState } from "react";
 import "react-calendar/dist/Calendar.css";
 import styles from "./Calendar.module.css";
 import { Text } from "@chakra-ui/react";
-import { Kumbh_Sans } from 'next/font/google';
+import { Kumbh_Sans } from "next/font/google";
 
-const kumbhSans = Kumbh_Sans({ subsets: ['latin'] });
+const kumbhSans = Kumbh_Sans({ subsets: ["latin"] });
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const CustomCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
   return (
     <div>
@@ -20,12 +33,20 @@ const CustomCalendar = () => {
         onChange={onChange}
         value={value}
         navigationLabel={({ date, locale, view }) => {
-            return <Text fontFamily={kumbhSans.style.fontFamily} fontSize="40px">{monthNames[date.getMonth()]} {date.getFullYear()}</Text>
+          return (
+            <Text fontFamily={kumbhSans.style.fontFamily} fontSize="40px">
+              {monthNames[date.getMonth()]} {date.getFullYear()}
+            </Text>
+          );
         }}
-        tileClassName={({ date, view}) => {
-            if (view === 'month') {
-                return styles.reactCalendar__tile;
-            }
+        tileDisabled={({ activeStartDate, date, view }) => {
+          return date.getMonth() !== activeStartDate.getMonth();
+        }}
+        tileClassName={({ date, view, activeStartDate }) => {
+          if (view === "month") {
+
+            return styles.reactCalendar__tile;
+          }
         }}
       />
     </div>
